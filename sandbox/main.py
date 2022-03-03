0e34e2f04ce7b2e91f2b414f4b6f09a020a0b039
Matěj Grabovský <matej@mgrabovsky.net>
2022-03-03 22:49:02 +0100

    sandbox: Improve tracing

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,4 +1,5 @@
 import csv
+from collections import defaultdict
 from dataclasses import dataclass
 from typing import cast
 
@@ -11,18 +12,16 @@ from electric_waltz.source import (
     PowerSource,
 )
 from electric_waltz.storage import EnergyStorage
-from electric_waltz.types import Energy, Power
+from electric_waltz.types import Power
 
 
 class Statistics:
-    total_flexible_generation: Energy = 0.0
-    total_inflexible_generation: Energy = 0.0
-    total_charging: Energy = 0.0
-    total_discharging: Energy = 0.0
-    total_export: Energy = 0.0
-    total_import: Energy = 0.0
-    total_dump: Energy = 0.0
-    total_shortage: Energy = 0.0
+    def __init__(self) -> None:
+        self.source_generation: dict[str, [Power]] = defaultdict(list)
+        self.storage_output: dict[str, list[Power]] = defaultdict(list)
+        self.net_import: list[Power] = []
+        # Shortage (positive) or dump (negative).
+        self.shortage: list[Power] = []
 
 
 @dataclass
@@ -42,10 +41,10 @@ def make_power_plant(kind: str, config) -> PowerSource:
 
     if kind in ("nuclear", "pv", "wind"):
         return NonDispatchableSource(
-            nominal=nominal_capacity, self_consumption=self_consumption
+            name=kind, nominal=nominal_capacity, self_consumption=self_consumption
         )
     return DispatchableSource(
-        nominal=nominal_capacity, self_consumption=self_consumption
+        name=kind, nominal=nominal_capacity, self_consumption=self_consumption
     )
 
 
@@ -59,6 +58,7 @@ def make_storage(kind: str, config) -> EnergyStorage:
     efficiency = config["storage"]["efficiency"][kind]
 
     return EnergyStorage(
+        name=kind,
         nominal=nominal_capacity,
         max_storage=max_energy,
         efficiency=efficiency,
@@ -66,14 +66,12 @@ def make_storage(kind: str, config) -> EnergyStorage:
 
 
 def run(config, states: list[World]) -> Statistics:
-    # Set up.
-    # ----------------------------------------------------------------
     # Collected statistics.
     stats = Statistics()
 
     # Inflexible power plants.
     nuclear = make_power_plant("nuclear", config)
-    pve = make_power_plant("pv", config)
+    pv = make_power_plant("pv", config)
     wind = make_power_plant("wind", config)
 
     # Flexible power plants.
@@ -88,7 +86,7 @@ def run(config, states: list[World]) -> Statistics:
 
     max_export: Power = config["cross_border"]["max_export"]
     max_import: Power = config["cross_border"]["max_import"]
-    total_losses: float = (
+    loss_factor: float = (
         config["consumption"]["transmission_loss"]
         + config["consumption"]["distribution_loss"]
     )
@@ -101,16 +99,15 @@ def run(config, states: list[World]) -> Statistics:
     for world in states:
         # Read current state.
         # ------------------------------------------------------------
-        total_consumption = world.demand * (1 + total_losses)
+        total_consumption = world.demand * (1 + loss_factor)
 
-        # Pass current utilisation down to `pve` and `wind`.
-        pve.utilisation = world.pv_utilisation
+        # Pass current utilisation down to `pv` and `wind`.
+        pv.utilisation = world.pv_utilisation
         wind.utilisation = world.wind_utilisation
 
         inflexible_generation = (
-            nuclear.net_generation + pve.net_generation + wind.net_generation
+            nuclear.net_generation + pv.net_generation + wind.net_generation
         )
-        stats.total_inflexible_generation += inflexible_generation
 
         # Dispatch what's necessary according to some rules.
         # ------------------------------------------------------------
@@ -121,41 +118,46 @@ def run(config, states: list[World]) -> Statistics:
             # Try charging storage.
             if surplus > 0:
                 charging = storage_dispatcher.charge_at(surplus)
-                stats.total_charging += charging
                 surplus -= charging
             # If storage is full, try export.
             export_power = min(surplus, max_export)
-            stats.total_export += export_power
+            stats.net_import.append(-export_power)
             # If export capacity is full, record dump/surplus.
-            stats.total_dump += max(0, surplus - export_power)
+            stats.shortage.append(-max(0, surplus - export_power))
         else:
             assert inflexible_generation < total_consumption
             deficit = total_consumption - inflexible_generation
             # Try discharging as much storage as needed and possible.
             discharging = storage_dispatcher.discharge_at(deficit)
-            stats.total_discharging += discharging
             deficit -= discharging
             # If consumption still dominates, try turning on flexible sources in
             # order of merit.
             if deficit > 0:
                 flexible_generation = flexible_dispatcher.dispatch_at(deficit)
-                stats.total_flexible_generation += flexible_generation
                 deficit -= flexible_generation
             else:
                 flexible_dispatcher.shut_down_all()
             # Try import if necessary.
             import_power = min(deficit, max_import)
-            stats.total_import += import_power
-            deficit -= import_power
-            assert deficit >= 0
+            stats.net_import.append(import_power)
             # Record shortage if the previous failed to satisfy demand.
-            if deficit > 0:
-                stats.total_shortage += deficit
+            stats.shortage.append(max(0, deficit - import_power))
+
+        stats.source_generation["nuclear"].append(nuclear.net_generation)
+        stats.source_generation["pv"].append(pv.net_generation)
+        stats.source_generation["wind"].append(wind.net_generation)
+        stats.source_generation["hydro"].append(hydro.net_generation)
+        stats.source_generation["biomass"].append(biomass.net_generation)
+        stats.source_generation["gas"].append(gas.net_generation)
+
+        stats.storage_output["pumped"].append(pumped.output)
+        stats.storage_output["battery"].append(battery.output)
+        stats.storage_output["p2g"].append(p2g.output)
 
     return stats
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     states: list[World] = []
     with open("sandbox/input_data.csv", encoding="utf-8") as csv_file:
         for row in csv.reader(csv_file):
@@ -163,7 +165,7 @@ if __name__ == '__main__':
                 World(
                     demand=float(row[0]),
                     pv_utilisation=float(row[1]),
-                    wind_utilisation=float(row[2])
+                    wind_utilisation=float(row[2]),
                 )
             )
 
@@ -173,19 +175,44 @@ if __name__ == '__main__':
 
     stats = run(config, states)
 
-    total_generation = (
-        stats.total_flexible_generation + stats.total_inflexible_generation
+    total_consumption = sum(s.demand for s in states)
+    total_flexible_generation = (
+        sum(stats.source_generation["hydro"])
+        + sum(stats.source_generation["biomass"])
+        + sum(stats.source_generation["gas"])
+    )
+    total_inflexible_generation = (
+        sum(stats.source_generation["nuclear"])
+        + sum(stats.source_generation["pv"])
+        + sum(stats.source_generation["wind"])
+    )
+    total_generation = total_flexible_generation + total_inflexible_generation
+
+    total_charging = -(
+        sum(output for output in stats.storage_output["pumped"] if output < 0)
+        + sum(output for output in stats.storage_output["battery"] if output < 0)
+        + sum(output for output in stats.storage_output["p2g"] if output < 0)
+    )
+    total_discharging = (
+        sum(output for output in stats.storage_output["pumped"] if output > 0)
+        + sum(output for output in stats.storage_output["battery"] if output > 0)
+        + sum(output for output in stats.storage_output["p2g"] if output > 0)
     )
 
-    print(f"Total net generation:        {total_generation:12,.0f} MWh")
-    print(f"Total flexible generation:   {stats.total_flexible_generation:12,.0f} MWh")
-    print(f"Total inflexible generation: {stats.total_inflexible_generation:12,.0f} MWh")
-    print(f"Total charging consumption:  {stats.total_charging:12,.0f} MWh")
-    print(f"Total discharging:           {stats.total_discharging:12,.0f} MWh")
-    print(f"Total export:                {stats.total_export:12,.0f} MWh")
-    print(f"Total import:                {stats.total_import:12,.0f} MWh")
-    print(f"Total dump:                  {stats.total_dump:12,.0f} MWh")
-    print(f"Total shortage:              {stats.total_shortage:12,.0f} MWh")
+    total_export = sum(-net_import for net_import in stats.net_import if net_import < 0)
+    total_import = sum(net_import for net_import in stats.net_import if net_import > 0)
 
-    print("\nTotal consumption:           {:12,.0f} MWh"
-          .format(sum(s.demand for s in states)))
+    total_dump = sum(-shortage for shortage in stats.shortage if shortage < 0)
+    total_shortage = sum(shortage for shortage in stats.shortage if shortage > 0)
+
+    print(f"Total net generation:        {total_generation:12,.0f} MWh")
+    print(f"Total flexible generation:   {total_flexible_generation:12,.0f} MWh")
+    print(f"Total inflexible generation: {total_inflexible_generation:12,.0f} MWh")
+    print(f"Total charging consumption:  {total_charging:12,.0f} MWh")
+    print(f"Total discharging:           {total_discharging:12,.0f} MWh")
+    print(f"Total export:                {total_export:12,.0f} MWh")
+    print(f"Total import:                {total_import:12,.0f} MWh")
+    print(f"Total dump:                  {total_dump:12,.0f} MWh")
+    print(f"Total shortage:              {total_shortage:12,.0f} MWh")
+
+    print(f"\nTotal consumption:           {total_consumption:12,.0f} MWh")
